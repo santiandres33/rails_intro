ce5f8815257ce96f82082fca0c57a56681af1a32
juan267 <j.gomez267@gmail.com>
2017-08-01 19:45:32 -0500

    add devise

--- associations_examples/config/routes.rb
+++ associations_examples/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   root 'books#index'
   resources :books, only: [:index, :show]
 
--- forms_examples/app/controllers/people_controller.rb
+++ forms_examples/app/controllers/people_controller.rb
@@ -1,7 +1,6 @@
 class PeopleController < ApplicationController
   protect_from_forgery :only => [:index]
 
-
   def index
     @people = Person.all
     if params[:q].present?
